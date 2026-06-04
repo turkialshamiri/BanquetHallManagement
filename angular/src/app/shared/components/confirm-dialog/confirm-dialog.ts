d662b53e82f504723afd75564ecbd47336b96074
turkialshamiri <turkimohammedalshamiri@gmail.com>
2026-06-04 17:34:26 +0300

    feat: add reusable confirmation dialog and hall deletion functionality

---
+++ angular/src/app/shared/components/confirm-dialog/confirm-dialog.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  imports: [MatIconModule],
+  standalone: true,
+  templateUrl: './confirm-dialog.html',
+  styleUrl: './confirm-dialog.scss'
+})
+export class ConfirmDialog {
+
+  readonly dialogRef = inject(MatDialogRef<ConfirmDialog>);
+  readonly data = inject(MAT_DIALOG_DATA);
+
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
+}
